87d1df1d19c06f307fe126e7cff3cc47a8e3b8ad
DimalMaloku1 <dm51522@ubt-uni.net>
2022-12-22 23:14:49 +0100

    Added MovieCategory

---
+++ E-Commerce-Etickets/eTickets/Data/Enmus/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace eTickets.Data.Enmus
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Cartoon,
+        Horror,
+    }
+}
